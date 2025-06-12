843e536a5e67fc1de22a51c71843468f62777e0d
K1ND2M3 <atommaihom2545@gmail.com>
2025-06-12 10:40:04 +0700

    update CORS

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: [
-    'http://localhost:3000', // สำหรับตอนพัฒนา
-    'https://queue-app-zeta.vercel.app' // << ใส่ URL ที่ได้จาก Vercel
+    'http://localhost:3000',
+    'https://queue-app-zeta.vercel.app'
   ]
 };
+
 app.use(cors(corsOptions));
 
 app.use(express.json());

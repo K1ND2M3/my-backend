d4468c0a286647a9416d7e8a229a8a93ef16754f
K1ND2M3 <atommaihom2545@gmail.com>
2025-06-12 14:06:10 +0700

    ข้อความสรุปการเปลี่ยนแปลง

--- server.js
+++ server.js
@@ -9,14 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'https://queue-app-zeta.vercel.app'
-  ]
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
